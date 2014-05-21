148589cac7c7abcb647d20ec0f2a129c60b2a462
Ed Fisher <ed@mrskin.com>
2014-05-21 15:25:07 -0500

    Set a sane default for the template cookbook, and allow it to be overriden.

--- resources/service.rb
+++ resources/service.rb
@@ -1,6 +1,7 @@
 actions :enable, :disable
 
 attribute :service_name, :name_attribute => true
+attribute :cookbook, :kind_of => String, :default => "xinetd"
 
 XinetdServiceHelpers::OPTIONS.each do |opt|
   attribute opt
